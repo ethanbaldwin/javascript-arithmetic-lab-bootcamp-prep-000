a9cf751634447833ceb33de253dd7579cd265bd8
Ethan A Baldwin <ethanbaldwin022@gmail.com>
2018-08-27 08:50:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,16 +15,25 @@ function divide(a,b) {
 }
 
 function inc(n) {
+<<<<<<< HEAD
   n += 1
+=======
+  n += n
+>>>>>>> fd44a8f5d1b12b2075d5259e55b1fbfd29416421
   return n
 }
 
 function dec(n) {
+<<<<<<< HEAD
   n-=1
+=======
+  n-=n
+>>>>>>> fd44a8f5d1b12b2075d5259e55b1fbfd29416421
   return n
 }
 
 function makeInt(n) {
+<<<<<<< HEAD
 return  parseInt(n, 10)
 }
 
@@ -32,3 +41,12 @@ function preserveDecimal(n) {
  return parseFloat(n)
 }
 
+=======
+  parseInt(n)
+}
+
+function makeInt(n) {
+  
+}
+ 
+>>>>>>> fd44a8f5d1b12b2075d5259e55b1fbfd29416421
